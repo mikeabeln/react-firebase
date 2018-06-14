6903391947c13b66691d31e9adb9232bf7826559
Mike Abeln <mikeabeln>
2018-06-14 15:26:27 -0700

    adding timer countdown to error404

--- src/views/error404/Error404.jsx
+++ src/views/error404/Error404.jsx
@@ -5,14 +5,41 @@ import { Redirect } from 'react-router-dom'
 
 class Error404 extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            error404: false,
+            timer: 5
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => this.setState({ error404: true }), 6000)
+        this.timerID = setInterval(() => this.tick(), 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        this.setState({
+            timer: this.state.timer - 1
+        })
+    }
+
     render() {
-	    return (
+
+        if (this.state.error404) {
+            return <Redirect to='/'/>
+        }
+        return (
 	        <div>
 	            {/* put 404 page content here */}
-	            <p>404 Not Found</p>
-	            <Redirect to='/'/>
+	            <p>404: Page Not Found! You will be redirected in {this.state.timer} second(s).</p>
 	        </div>
-	    )
+
+        )
     }
 }
 
