43a96a01c2ac896db8fa84b66cfc4d1e18ccf560
Mike Abeln <mikeabeln>
2018-06-20 18:29:49 -0700

    removing history from index.jsx

--- client/index.jsx
+++ client/index.jsx
@@ -7,16 +7,6 @@ import './index.scss'
 
 import Routes from './routes.jsx'
 
-// const history = createBrowserHistory()
-
-// history.listen(() => {
-//     window.scrollTo(0, 0)
-//     ReactDOM.renderApp()
-// })
-
-// console.log(history)
-
-
 export const renderApp = () => {
     ReactDOM.render((<Routes/>), document.getElementById('root'))
 }
